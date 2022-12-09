3d65172fb7b43d40de9e481e1c7932f1db80c488
Kerill <96432815+stevenson67@users.noreply.github.com>
2022-12-09 19:35:13 +0500

    b
    b

---
+++ js/marquee.js
@@ -0,0 +1,13 @@
+/*$('.tarif__item-text').marquee({
+    duration: 5000,
+    delayBeforeStart: 0,
+    duplicated: true,
+    startVisible: true
+  });
+
+$(".tarif__item-text").hover(function () { 
+    this.stop();
+}, function () {
+    this.start();
+});
+ */
